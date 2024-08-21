ebe9945dda81e3746a9932d8728f4917eed7d6f1
CubillosD <diegoandcubillos@gmail.com>
2024-08-20 20:10:32 -0500

    primer commit

---
+++ src/Main.kt
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+        //val op = Operaciones(5.0,7.2)
+        //op.suma()
+        //op.resta()
+        //println("El resultado de la division es ${op.div()}")
+        //op.raiz(12.3)
+        //println("EL cuadrato es ${op.cuadrado(4.0)}")
+
+}
+
---
+++ src/Operaciones.kt
@@ -0,0 +1,27 @@
+class Operaciones(var n1: Double ,var n2: Double) {
+
+    fun suma() {
+        var sum = n1 + n2
+        println("El resultado de la suma es ${sum}")
+
+    }
+
+    fun resta() {
+        var res = n1 - n2
+        println("El resultado de la resta es ${res}")
+    }
+
+    fun div(): Double {
+        return n1 / n2
+    }
+
+    fun raiz(X:Double){
+    var r = Math.sqrt(X)
+    println("EL resultado de la raiz es ${r}")
+    }
+
+    fun cuadrado(C:Double):Double{
+        return Math.pow(C,2.5)
+    }
+
+}
